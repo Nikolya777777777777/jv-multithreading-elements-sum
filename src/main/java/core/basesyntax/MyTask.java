bad9e3bfa05c9ceb29fedeceb8e543a56de2b874
mykola <nikolya.cr@gmail.com>
2025-07-27 21:20:16 +0200

    first commit

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -5,7 +5,6 @@ import java.util.concurrent.RecursiveTask;
 public class MyTask extends RecursiveTask<Long> {
     private int startPoint;
     private int finishPoint;
-
     public MyTask(int startPoint, int finishPoint) {
         this.startPoint = startPoint;
         this.finishPoint = finishPoint;
