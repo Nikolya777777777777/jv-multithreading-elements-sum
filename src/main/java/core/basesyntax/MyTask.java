d69f5cf95648c92700ac80572846a2d22183d840
mykola <nikolya.cr@gmail.com>
2025-07-30 14:59:12 +0200

    final result

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -5,12 +5,24 @@ import java.util.concurrent.RecursiveTask;
 public class MyTask extends RecursiveTask<Long> {
     private int startPoint;
     private int finishPoint;
+    private Long finalResult = 0L;
+
     public MyTask(int startPoint, int finishPoint) {
         this.startPoint = startPoint;
         this.finishPoint = finishPoint;
     }
+
     @Override
     protected Long compute() {
-        return null;
+        if (finishPoint < startPoint) {
+            return 0L;
+        }
+        if (finishPoint == 0) {
+            return 0L;
+        }
+        for (int i = startPoint; i < finishPoint; i++) {
+            finalResult += (long) i;
+        }
+        return finalResult;
     }
 }
